6587c688f5e2675a56590d5f673fa335059f8d9c
Rasel <mdraselm413@gmail.com>
2022-09-13 21:44:52 +0600

    fundamental added

--- fundamentals.js
+++ fundamentals.js
@@ -1,4 +1,15 @@
 /* 
-1. How to declear a variable.
-2. 
+1. How to declear a variable using let and const
+    const fatherName = "Arnold devid Mouder"
+    lett seasone = "rainy";
+    seasone = "winter";
+2. Conditional Statement basic 6 ( >, <, ===, !==, >=, <= ), multiple condition ( &&, ||)
+3. Arrey Declaration [imdex, length, push, pop] 
+4. Loop ( for loop, while loop )
+5. function Declaration
+7. Object Declaration{}
+    3 ways to access property by name
+    7.1 object.propertyname  -> direct access
+    7.2 object["property name"]  -> access via property name string
+    7.3 object[variableName] -> access via property name in a variable
 */
