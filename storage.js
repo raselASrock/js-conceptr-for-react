f61b9a84021b7acfc5745e98004c3d8820a2f140
Rasel <mdraselm413@gmail.com>
2022-09-14 22:30:11 +0600

    storage added

--- storage.js
+++ storage.js
@@ -1,2 +1,15 @@
 console.log('Storage added');
-localStorage.setItem()
+localStorage.setItem('userId', 87952214);
+const addToLocalStorage = () => {
+    const idInput = document.getElementById('storage-id');
+    const id = idInput.value ;
+    const valueInput = document.getElementById('storage-value');
+    const value = valueInput.value;
+
+// Add to local Storage
+if (id && value) {
+    localStorage.setItem(id, value)}
+idInput.value = '';
+valueInput.value = '';
+
+}
